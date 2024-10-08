baf5b3fb54db972bf7ba8acc7033cf2a86842160
FedericoBustaffa <kiko.bustaffa@gmail.com>
2024-10-08 12:19:40 +0200

    rename

--- src/ppga/solver/__init__.py
+++ src/ppga/solver/__init__.py
@@ -1,3 +1,3 @@
 from ppga.solver.genetic_solver import GeneticSolver
 from ppga.solver.pipe_solver import PipeGeneticSolver
-from ppga.solver.queued_solver import QueuedGeneticSolver
+from ppga.solver.queue_solver import QueuedGeneticSolver
