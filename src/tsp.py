fa51f09994237efd6b3bd3fd9c3a10cb658732dd
FedericoBustaffa <kiko.bustaffa@gmail.com>
2024-10-08 19:20:44 +0200

    eval sleep

--- src/tsp.py
+++ src/tsp.py
@@ -32,7 +32,7 @@ def evaluate(chromosome, towns: list[Town]) -> tuple:
     for i in range(len(chromosome) - 1):
         total_distance += distance(towns[chromosome[i]], towns[chromosome[i + 1]])
 
-    # time.sleep(0.00005)
+    time.sleep(0.00005)
 
     return (total_distance,)
 
