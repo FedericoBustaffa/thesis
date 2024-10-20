2ca01fe66e5334f51fec3637b9d24cce131b125d
Federico Bustaffa <62218166+FedericoBustaffa@users.noreply.github.com>
2024-10-21 01:00:02 +0200

    add  total replacement and fix merge replacement

--- src/ppga/tools/replace.py
+++ src/ppga/tools/replace.py
@@ -1,6 +1,12 @@
 from ppga.base import Individual
 
 
+def total(
+    population: list[Individual], offsprings: list[Individual]
+) -> list[Individual]:
+    return offsprings
+
+
 def merge(
     population: list[Individual], offsprings: list[Individual]
 ) -> list[Individual]:
@@ -8,26 +14,14 @@ def merge(
     offsprings = sorted(offsprings, reverse=True)
 
     next_generation = []
-    index = 0
     index1 = 0
     index2 = 0
-
-    while (
-        index < len(population)
-        and index1 < len(population)
-        and index2 < len(offsprings)
-    ):
+    for index in range(len(population)):
         if population[index1] > offsprings[index2]:
             next_generation.append(population[index1])
             index1 += 1
         else:
             next_generation.append(offsprings[index2])
             index2 += 1
-        index += 1
-
-    if index1 >= len(population):
-        return next_generation
-    elif index2 >= len(offsprings):
-        next_generation[index:] = population[index1 : len(population) - index2]
 
     return next_generation
