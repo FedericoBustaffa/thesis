90dc786167db893242f196b107f96190ae37126e
Federico Bustaffa <62218166+FedericoBustaffa@users.noreply.github.com>
2024-10-27 16:30:03 +0100

    selection

--- src/ppga/tools/__init__.py
+++ src/ppga/tools/__init__.py
@@ -7,13 +7,14 @@ from ppga.tools.crossover import (
 from ppga.tools.generation import gen_permutation, gen_repetition
 from ppga.tools.mutation import bit_flip, mut_shuffle, rotation
 from ppga.tools.replacement import merge, total
-from ppga.tools.selection import roulette, tournament
+from ppga.tools.selection import sel_roulette, sel_tournament, sel_truncation
 
 __all__ = [
     "gen_repetition",
     "gen_permutation",
-    "tournament",
-    "roulette",
+    "sel_truncation",
+    "sel_tournament",
+    "sel_roulette",
     "cx_one_point",
     "cx_one_point_ordered",
     "cx_two_points",
--- src/tsp.py
+++ src/tsp.py
@@ -47,8 +47,8 @@ def main(argv: list[str]):
 
     toolbox = base.ToolBox()
     toolbox.set_weights((-1.0,))
-    toolbox.set_generation(tools.gen_permutation, range(len(towns)), len(towns))
-    toolbox.set_selection(tools.tournament, tournsize=2)
+    toolbox.set_generation(tools.gen_permutation, range(len(towns)))
+    toolbox.set_selection(tools.sel_tournament, tournsize=2)
     toolbox.set_crossover(tools.cx_one_point_ordered)
     toolbox.set_mutation(tools.rotation)
     toolbox.set_evaluation(evaluate, towns)
