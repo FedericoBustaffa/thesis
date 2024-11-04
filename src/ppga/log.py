ab62e4c1b24b3bb2bdea6bf735f4a3aae2b6a024
Federico Bustaffa <62218166+FedericoBustaffa@users.noreply.github.com>
2024-11-04 13:24:04 +0100

    loggin

--- src/ppga/log.py
+++ src/ppga/log.py
@@ -1,6 +1,6 @@
 import logging
 
-from colorama import Fore
+from colorama import Back, Fore
 
 FMT = "[{levelname}] {name}: {message}"
 
@@ -10,6 +10,7 @@ SUCCESS = 15
 INFO = logging.INFO
 WARNING = logging.WARNING
 ERROR = logging.ERROR
+CRITICAL = logging.CRITICAL
 
 logging.addLevelName(15, "SUCCESS")
 
@@ -19,7 +20,7 @@ FORMATS = {
     logging.INFO: Fore.WHITE + FMT + Fore.RESET,
     logging.WARNING: Fore.YELLOW + FMT + Fore.RESET,
     logging.ERROR: Fore.RED + FMT + Fore.RESET,
-    logging.CRITICAL: Fore.LIGHTRED_EX + FMT + Fore.RESET,
+    logging.CRITICAL: Back.RED + Fore.WHITE + FMT + Fore.RESET + Back.RESET,
 }
 
 
@@ -30,6 +31,8 @@ class ColorFormatter(logging.Formatter):
 
 
 class Logger(logging.Logger):
+    """Wrapper around the Logger class of the `logging` module"""
+
     def __init__(self, name, level):
         super().__init__(name, level)
 
@@ -37,13 +40,28 @@ class Logger(logging.Logger):
         super().log(SUCCESS, message)
 
 
-logger = Logger("USER", INFO)
+core_logger = Logger("CORE", WARNING)
+formatter = ColorFormatter()
+handler = logging.StreamHandler()
+handler.setLevel(WARNING)
+handler.setFormatter(formatter)
+core_logger.addHandler(handler)
+
+
+user_logger = Logger("USER", INFO)
 formatter = ColorFormatter()
 handler = logging.StreamHandler()
 handler.setLevel(INFO)
 handler.setFormatter(formatter)
-logger.addHandler(handler)
+user_logger.addHandler(handler)
 
 
-def getLogger() -> Logger:
-    return logger
+def set_core_level(level: int = WARNING):
+    """Set the core logger level"""
+    core_logger.setLevel(level)
+
+
+def getLogger(level: int = INFO) -> Logger:
+    """Provides a logger for the user with the given log level"""
+    user_logger.setLevel(level)
+    return user_logger
