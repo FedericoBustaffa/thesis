1f3fe1fb0ed1e261485713d1045da65d95280ee5
Federico Bustaffa <62218166+FedericoBustaffa@users.noreply.github.com>
2024-11-04 15:27:09 +0100

    mu lambda algorithm
    decide how many individuals select and how many offsprings generate per
    generation

---
+++ src/ppga/algorithms/mu_lambda.py
@@ -0,0 +1,90 @@
+import psutil
+from tqdm import tqdm
+
+from ppga.algorithms.reproduction import reproduction
+from ppga.algorithms.worker import Worker
+from ppga.base import HallOfFame, Statistics, ToolBox
+
+
+def mu_lambda(
+    toolbox: ToolBox,
+    population_size: int,
+    mu: int,
+    lam: int,
+    cxpb: float,
+    mutpb: float,
+    max_generations: int,
+    hall_of_fame: None | HallOfFame = None,
+):
+    stats = Statistics()
+
+    population = toolbox.generate(population_size)
+    for g in tqdm(range(max_generations), desc="generations", ncols=80):
+        chosen = toolbox.select(population, mu)
+        offsprings = reproduction(chosen, toolbox, lam, cxpb, mutpb)
+
+        for offspring in offsprings:
+            offspring = toolbox.evaluate(offspring)
+
+        stats.update_evals(len(offsprings))
+
+        population = toolbox.replace(population, offsprings)
+
+        stats.update(population)
+
+        if hall_of_fame is not None:
+            hall_of_fame.update(population)
+
+    return population, stats
+
+
+def parallel_mu_lambda(
+    toolbox: ToolBox,
+    population_size: int,
+    mu: int,
+    lam: int,
+    cxpb: float,
+    mutpb: float,
+    max_generations: int,
+    hall_of_fame: None | HallOfFame = None,
+):
+    stats = Statistics()
+
+    # only use the physical cores
+    workers_num = psutil.cpu_count(logical=False)
+    assert workers_num is not None
+
+    workers = [Worker(toolbox, lam, cxpb, mutpb) for _ in range(workers_num)]
+
+    population = toolbox.generate(population_size)
+    for g in tqdm(range(max_generations), desc="generations", ncols=80):
+        chosen = toolbox.select(population, mu)
+        chunksize = len(chosen) // workers_num
+        carry = len(chosen) % workers_num
+
+        for i in range(carry):
+            workers[i].send(chosen[i * chunksize : i * chunksize + chunksize + 1])
+
+        for i in range(carry, workers_num, 1):
+            workers[i].send(chosen[i * chunksize : i * chunksize + chunksize])
+
+        # keep only the worst time for each worker
+        offsprings = []
+        evals = []
+        for worker in workers:
+            offsprings_chunk = worker.recv()
+            offsprings.extend(offsprings_chunk)
+            evals.append(len(offsprings_chunk))
+
+        population = toolbox.replace(population, offsprings)
+
+        stats.update(population)
+        stats.update_multievals(evals)
+
+        if hall_of_fame is not None:
+            hall_of_fame.update(population)
+
+    for w in workers:
+        w.join()
+
+    return population, stats
