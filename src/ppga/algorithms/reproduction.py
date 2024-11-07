b66cd80d8227311c5bad142d87b833c3f5e9ad4a
Federico Bustaffa <62218166+FedericoBustaffa@users.noreply.github.com>
2024-11-07 16:53:52 +0100

    reproduction with cloning

--- src/ppga/algorithms/reproduction.py
+++ src/ppga/algorithms/reproduction.py
@@ -3,12 +3,13 @@ import random
 from ppga.base import Individual, ToolBox
 
 
-def reproduction(chosen: list[Individual], toolbox: ToolBox, cxpb: float, mutpb: float):
+def reproduction(
+    chosen: list[Individual], toolbox: ToolBox, cxpb: float, mutpb: float
+) -> list[Individual]:
     offsprings = []
-    chosen = [toolbox.clone(i) for i in chosen]
     for i in range(0, len(chosen), 2):
+        father, mother = random.choices(chosen, k=2)
         if random.random() <= cxpb:
-            father, mother = random.choices(chosen, k=2)
             offspring1, offspring2 = toolbox.crossover(father, mother)
 
             if random.random() <= mutpb:
@@ -18,5 +19,7 @@ def reproduction(chosen: list[Individual], toolbox: ToolBox, cxpb: float, mutpb:
                 offspring2 = toolbox.mutate(offspring2)
 
             offsprings.extend([offspring1, offspring2])
+        else:
+            offsprings.extend([toolbox.clone(father), toolbox.clone(mother)])
 
     return offsprings
