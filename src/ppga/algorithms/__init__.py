fce73ab436a4eed87d299ba335b7c235e15bd28f
Federico Bustaffa <62218166+FedericoBustaffa@users.noreply.github.com>
2024-11-07 16:52:52 +0100

    imports

--- src/ppga/algorithms/__init__.py
+++ src/ppga/algorithms/__init__.py
@@ -1,12 +1,11 @@
-from ppga.algorithms.mu_lambda import mu_lambda, parallel_mu_lambda
+from ppga.algorithms.elitist import elitist, pelitist
+from ppga.algorithms.generational import generational, pgenerational
 from ppga.algorithms.reproduction import reproduction
-from ppga.algorithms.simple import psga, sga
 
 __all__ = [
-    "psga",
     "reproduction",
-    "sga",
-    "reproduction",
-    "mu_lambda",
-    "parallel_mu_lambda",
+    "generational",
+    "pgenerational",
+    "elitist",
+    "pelitist",
 ]
