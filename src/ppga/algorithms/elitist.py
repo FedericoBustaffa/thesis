62871f612dd31f995fcdb986aecd7b3315ed9fa6
Federico Bustaffa <62218166+FedericoBustaffa@users.noreply.github.com>
2024-11-08 17:27:27 +0100

    wrapper of custom

--- src/ppga/algorithms/elitist.py
+++ src/ppga/algorithms/elitist.py
@@ -1,119 +1,51 @@
-import psutil
-
 from ppga import log, tools
-from ppga.algorithms.reproduction import reproduction
-from ppga.algorithms.worker import Worker
-from ppga.base import HallOfFame, Statistics, ToolBox
+from ppga.algorithms.custom import custom, pcustom
+from ppga.base import HallOfFame, ToolBox
 
 
 def elitist(
     toolbox: ToolBox,
     population_size: int,
-    cxpb: float,
-    mutpb: float,
-    max_generations: int,
+    keep: float = 0.5,
+    cxpb: float = 0.8,
+    mutpb: float = 0.2,
+    max_generations: int = 50,
     hall_of_fame: None | HallOfFame = None,
     log_level: str | int = log.WARNING,
 ):
-    toolbox.set_replacement(tools.elitist)
+    toolbox.set_replacement(tools.elitist, keep=keep)
 
-    stats = Statistics()
-    logger = log.getCoreLogger(log_level)
-
-    population = toolbox.generate(population_size)
-    logger.info(f"\t{'gen':15s}{'evals':15s}")
-
-    for g in range(max_generations):
-        # select individuals for reproduction
-        chosen = toolbox.select(population, population_size)
-
-        # perform reproduction and mutation
-        offsprings = reproduction(chosen, toolbox, cxpb, mutpb)
-
-        # evaluate the individuals with invalid fitness
-        evals = sum([1 for i in offsprings if not i.invalid])
-        offsprings = list(map(toolbox.evaluate, offsprings))
-
-        # elitist replacement
-        population = toolbox.replace(population, offsprings)
-
-        # update the Hall of Fame if present
-        if hall_of_fame is not None:
-            hall_of_fame.update(population)
-
-        # update the stats
-        stats.update(population)
-        stats.update_evals(evals)
-
-        logger.info(f"\t{g:<15d}{len(offsprings):<15d}")
-
-    return population, stats
+    return custom(
+        toolbox,
+        population_size,
+        keep,
+        cxpb,
+        mutpb,
+        max_generations,
+        hall_of_fame,
+        log_level,
+    )
 
 
 def pelitist(
     toolbox: ToolBox,
     population_size: int,
-    cxpb: float,
-    mutpb: float,
-    max_generations: int,
+    keep: float = 0.5,
+    cxpb: float = 0.8,
+    mutpb: float = 0.2,
+    max_generations: int = 50,
     hall_of_fame: None | HallOfFame = None,
     log_level: str | int = log.WARNING,
 ):
-    toolbox.set_replacement(tools.elitist)
+    toolbox.set_replacement(tools.elitist, keep=keep)
 
-    stats = Statistics()
-
-    logger = log.getCoreLogger(log_level)
-
-    # only use the physical cores
-    workers_num = psutil.cpu_count(logical=False)
-
-    # dinamically resize the chunksize
-    if population_size < workers_num:
-        workers_num = population_size
-
-    chunksize = population_size // workers_num
-    carry = population_size % workers_num
-    logger.debug(f"chunksize: {chunksize}")
-    logger.debug(f"carry: {carry}")
-    logger.debug(f"workers: {workers_num}")
-
-    workers = [Worker(toolbox, cxpb, mutpb, log_level) for _ in range(workers_num)]
-
-    population = toolbox.generate(population_size)
-    logger.info(f"\t{'gen':15s}{'evals':15s}")
-    for g in range(max_generations):
-        # select individuals for reproduction
-        chosen = toolbox.select(population, population_size)
-
-        # perform crossover, mutation and evaluation in parallel
-        for i in range(carry):
-            workers[i].send(chosen[i * chunksize : i * chunksize + chunksize + 1])
-
-        for i in range(carry, workers_num, 1):
-            workers[i].send(chosen[i * chunksize : i * chunksize + chunksize])
-
-        # results of parallel computation
-        offsprings = []
-        evals = []
-        for worker in workers:
-            offsprings_chunk = worker.recv()
-            offsprings.extend(offsprings_chunk)
-            evals.append(len(offsprings_chunk))
-
-        population = toolbox.replace(population, offsprings)
-
-        # update stats
-        stats.update(population)
-        stats.update_multievals(evals)
-
-        logger.info(f"\t{g:<15d}{len(offsprings):<15d}")
-
-        # update Hall of Fame if present
-        if hall_of_fame is not None:
-            hall_of_fame.update(population)
-
-    for w in workers:
-        w.join()
-
-    return population, stats
+    return pcustom(
+        toolbox,
+        population_size,
+        keep,
+        cxpb,
+        mutpb,
+        max_generations,
+        hall_of_fame,
+        log_level,
+    )
--- src/ppga/algorithms/generational.py
+++ src/ppga/algorithms/generational.py
@@ -1,116 +1,49 @@
-import psutil
-
 from ppga import log, tools
-from ppga.algorithms.reproduction import reproduction
-from ppga.algorithms.worker import Worker
-from ppga.base import HallOfFame, Statistics, ToolBox
+from ppga.algorithms.custom import custom, pcustom
+from ppga.base import HallOfFame, ToolBox
 
 
 def generational(
     toolbox: ToolBox,
     population_size: int,
-    cxpb: float,
-    mutpb: float,
-    max_generations: int,
+    cxpb: float = 0.8,
+    mutpb: float = 0.2,
+    max_generations: int = 50,
     hall_of_fame: None | HallOfFame = None,
     log_level: str | int = log.WARNING,
 ):
     toolbox.set_replacement(tools.total)
 
-    stats = Statistics()
-    logger = log.getCoreLogger(log_level)
-
-    # generate the initial population
-    population = toolbox.generate(population_size)
-
-    logger.info(f"\t{'gen':15s}{'evals':15s}")
-
-    for g in range(max_generations):
-        # select individuals for reproduction
-        chosen = toolbox.select(population, population_size)
-
-        # perform crossover and mutation
-        offsprings = reproduction(chosen, toolbox, cxpb, mutpb)
-
-        # evaluate the individuals with invalid fitness
-        for i in range(len(offsprings)):
-            if offsprings[i].invalid:
-                offsprings[i] = toolbox.evaluate(offsprings[i])
-
-        # elitist replacement
-        population = toolbox.replace(population, offsprings)
-
-        # update the Hall of Fame if present
-        if hall_of_fame is not None:
-            hall_of_fame.update(population)
-
-        # update the stats
-        stats.update(population)
-        stats.update_evals(len([i for i in offsprings if i.invalid]))
-
-        logger.info(f"\t{g:<15d}{len(offsprings):<15d}")
-
-    return population, stats
+    return custom(
+        toolbox=toolbox,
+        population_size=population_size,
+        keep=0.0,
+        cxpb=cxpb,
+        mutpb=mutpb,
+        max_generations=max_generations,
+        hall_of_fame=hall_of_fame,
+        log_level=log_level,
+    )
 
 
 def pgenerational(
     toolbox: ToolBox,
     population_size: int,
-    cxpb: float,
-    mutpb: float,
-    max_generations: int,
+    cxpb: float = 0.8,
+    mutpb: float = 0.2,
+    max_generations: int = 50,
     hall_of_fame: None | HallOfFame = None,
     log_level: str | int = log.WARNING,
 ):
     toolbox.set_replacement(tools.total)
 
-    stats = Statistics()
-
-    logger = log.getCoreLogger(log_level)
-
-    # only use the physical cores
-    workers_num = psutil.cpu_count(logical=False)
-
-    # dinamically resize the chunksize
-    if population_size < workers_num:
-        workers_num = population_size
-
-    chunksize = population_size // workers_num
-    carry = population_size % workers_num
-
-    workers = [Worker(toolbox, cxpb, mutpb, log_level) for _ in range(workers_num)]
-
-    population = toolbox.generate(population_size)
-
-    logger.info(f"\t{'gen':15s}{'evals':15s}")
-
-    for g in range(max_generations):
-        chosen = toolbox.select(population, population_size)
-
-        for i in range(carry):
-            workers[i].send(chosen[i * chunksize : i * chunksize + chunksize + 1])
-
-        for i in range(carry, workers_num, 1):
-            workers[i].send(chosen[i * chunksize : i * chunksize + chunksize])
-
-        offsprings = []
-        evals = []
-        for worker in workers:
-            offsprings_chunk = worker.recv()
-            offsprings.extend(offsprings_chunk)
-            evals.append(len(offsprings_chunk))
-
-        population = toolbox.replace(population, offsprings)
-
-        stats.update(population)
-        stats.update_multievals(evals)
-
-        logger.info(f"\t{g:<15d}{len(offsprings):<15d}")
-
-        if hall_of_fame is not None:
-            hall_of_fame.update(population)
-
-    for w in workers:
-        w.join()
-
-    return population, stats
+    return pcustom(
+        toolbox=toolbox,
+        population_size=population_size,
+        keep=0.0,
+        cxpb=cxpb,
+        mutpb=mutpb,
+        max_generations=max_generations,
+        hall_of_fame=hall_of_fame,
+        log_level=log_level,
+    )
