45c96dfa33b04374993c94171aaf8597bb0f194c
FedericoBustaffa <kiko.bustaffa@gmail.com>
2024-11-30 19:51:18 +0100

    animation

---
+++ sources/animations/genetic.py
@@ -0,0 +1,125 @@
+import warnings
+
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.collections import PathCollection
+from numpy import linalg, random
+
+from ppga import base
+from ppga.algorithms import batch
+
+warnings.filterwarnings("ignore")
+
+
+# generation by copy
+def generate_copy(point: np.ndarray) -> np.ndarray:
+    return point.copy()
+
+
+# mutate
+def normal_mutate(
+    individual: np.ndarray, mu: np.ndarray, sigma: np.ndarray, indpb: float = 0.5
+):
+    for i in range(len(individual)):
+        if random.random() <= indpb:
+            individual[i] = random.normal(mu[i], sigma[i])
+
+    return individual
+
+
+def normal_mutate_gradual(
+    individual, mu, sigma, alpha: float = 0.1, indpb: float = 0.5
+):
+    for i in range(len(individual)):
+        if random.random() <= indpb:
+            individual[i] -= random.normal(loc=mu[i], scale=sigma[i]) * alpha
+
+    return individual
+
+
+def evaluate(
+    chromosome: np.ndarray,
+    point: np.ndarray,
+    target: np.ndarray,
+    blackbox,
+    epsilon: float = 0.0,
+    alpha: float = 0.0,
+):
+    assert alpha >= 0.0 and alpha <= 1.0
+
+    # classification
+    synth_class = blackbox.predict(chromosome.reshape(1, -1))
+
+    # compute euclidean distance
+    distance = linalg.norm(chromosome - point, ord=2)
+
+    # compute classification penalty
+    right_target = 1.0 - alpha if target == synth_class[0] else alpha
+
+    # check the epsilon distance
+    if distance <= epsilon:
+        return (np.inf,)
+
+    return (distance / right_target,)
+
+
+def plot_hof(
+    X: np.ndarray,
+    y: np.ndarray,
+    hof: np.ndarray,
+    point: np.ndarray | None = None,
+    outcome: int | None = None,
+):
+    x0 = X.T[0][y == 0]
+    x1 = X.T[0][y == 1]
+    y0 = X.T[1][y == 0]
+    y1 = X.T[1][y == 1]
+
+    plt.clf()
+    plt.scatter(x0, y0, c="b", ec="w")
+    plt.scatter(x1, y1, c="r", ec="w")
+    if point is not None:
+        plt.scatter(
+            point[0], point[1], c="b" if outcome == 0 else "r", ec="w", marker="X"
+        )
+
+    plt.scatter(hof.T[0], hof.T[1], c="y", ec="w")
+    plt.pause(0.0001)
+
+
+def animate(frame, scatter: PathCollection):
+    # scatter.set_offsets()
+    return (scatter,)
+
+
+def run(
+    X,
+    y,
+    point,
+    outcome,
+    toolbox: base.ToolBox,
+    population_size: int,
+    max_generations: int,
+):
+    hof = base.HallOfFame(population_size)
+
+    population = toolbox.generate(population_size)
+    plt.figure(figsize=(16, 9))
+    plt.title("Dataset")
+    plt.xlabel("feature 1")
+    plt.ylabel("feature 2")
+    for g in range(max_generations):
+        print(g)
+        selected = toolbox.select(population, population_size)
+        couples = batch.mating(selected)
+        offsprings = batch.crossover(couples, toolbox, 0.8)
+        offsprings = batch.mutation(offsprings, toolbox, 0.2)
+        offsprings = batch.evaluation(offsprings, toolbox)
+        population = toolbox.replace(population, offsprings)
+
+        hof.update(population)
+
+        best_points = np.asarray([i.chromosome for i in hof])
+        plot_hof(X, y, best_points, point, outcome)
+
+    plt.show()
---
+++ sources/animations/main.py
@@ -0,0 +1,92 @@
+import genetic
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+
+from ppga import base, tools
+
+
+def plot_dataset(
+    X: np.ndarray,
+    y: np.ndarray,
+    point: np.ndarray | None = None,
+    outcome: int | None = None,
+):
+    x0 = X.T[0][y == 0]
+    x1 = X.T[0][y == 1]
+    y0 = X.T[1][y == 0]
+    y1 = X.T[1][y == 1]
+
+    plt.figure(figsize=(16, 9))
+    plt.title("Dataset")
+    plt.xlabel("feature 1")
+    plt.ylabel("feature 2")
+    plt.scatter(x0, y0, c="b", ec="w")
+    plt.scatter(x1, y1, c="r", ec="w")
+    if point is not None:
+        plt.scatter(
+            point[0], point[1], c="b" if outcome == 0 else "r", ec="w", marker="X"
+        )
+    plt.show()
+
+
+def main():
+    # create dataset
+    X, y = make_classification(
+        n_samples=200,
+        n_features=2,
+        n_informative=2,
+        n_redundant=0,
+        n_repeated=0,
+        n_classes=2,
+        n_clusters_per_class=1,
+        shuffle=True,
+        random_state=0,
+    )
+
+    X = np.asarray(X)
+    y = np.asarray(y)
+    plot_dataset(X, y)
+
+    # split
+    X_train, X_test, y_train, _ = train_test_split(X, y, test_size=0.2, random_state=0)
+    X_train = np.asarray(X_train)
+    y_train = np.asarray(y_train)
+    X_test = np.asarray(X_test)
+
+    # train and test model
+    classifier = MLPClassifier(max_iter=2000)
+    classifier.fit(X_train, y_train)
+    outcomes = np.asarray(classifier.predict(X_test))
+    point = X_test[0]
+    target = (outcomes[0] + 1) % 2
+    plot_dataset(X_test, outcomes, point, outcomes[0])
+
+    # run the genetic algorithm on one point
+    toolbox = base.ToolBox()
+    toolbox.set_weights(weights=(-1.0,))
+    toolbox.set_generation(genetic.generate_copy, point=point)
+    toolbox.set_selection(tools.sel_tournament, tournsize=3)
+    toolbox.set_crossover(tools.cx_one_point)
+    toolbox.set_mutation(
+        genetic.normal_mutate,
+        mu=X_test.mean(axis=0),
+        sigma=X_test.std(axis=0) * 0.5,
+        indpb=0.5,
+    )
+    toolbox.set_evaluation(
+        genetic.evaluate,
+        point=point,
+        target=target,
+        blackbox=classifier,
+        epsilon=0.0,
+        alpha=0.0,
+    )
+    toolbox.set_replacement(tools.elitist, keep=0.1)
+    genetic.run(X_test, outcomes, point, outcomes[0], toolbox, 500, 100)
+
+
+if __name__ == "__main__":
+    main()
