2d3eacc9270574ab040bbff6354806093f86cdba
FedericoBustaffa <kiko.bustaffa@gmail.com>
2024-12-05 15:43:02 +0000

    logging

--- sources/explain/simulation.py
+++ sources/explain/simulation.py
@@ -6,21 +6,26 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.svm import SVC
 
 from explain import explain
+from ppga import log
 
 if __name__ == "__main__":
     blackboxes = [RandomForestClassifier(), SVC(), MLPClassifier()]
 
-    filepaths = os.listdir("./datasets/")
+    filepaths = sorted(os.listdir("./datasets/"))
+    logger = log.getUserLogger()
+    logger.setLevel("INFO")
 
     for filepath in filepaths:
         for bb in blackboxes:
             predictions = blackbox.make_predictions(bb, f"datasets/{filepath}")
 
+            name = str(bb).removesuffix("()")
+            logger.info(f"{filepath} explained with {name}")
+
             X = predictions[[k for k in predictions if k != "outcome"]].to_numpy()
             y = predictions["outcome"].to_numpy()
 
             stats = explain(X, y, bb, 1000)
-            name = str(bb).removesuffix("()")
 
             params = filepath.split("_")
             samples = int(params[1])
