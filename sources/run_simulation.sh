d0cfbab15eb30e6f097ede26093b4a496a7ee4db
FedericoBustaffa <kiko.bustaffa@gmail.com>
2024-12-18 18:35:44 +0100

    more professional run script

--- sources/run_simulation.sh
+++ sources/run_simulation.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-python -m scoop -n 2 benchmarks/deap_scoop.py 2
-python -m scoop -n 4 benchmarks/deap_scoop.py 4
-python -m scoop -n 8 benchmarks/deap_scoop.py 8
-python -m scoop -n 16 benchmarks/deap_scoop.py 16
-python -m scoop -n 32 benchmarks/deap_scoop.py 32
+workers=(2 4 8 16 32)
+
+for w in ${workers[@]}
+do
+    python -m scoop -n "$w" benchmarks/deap_scoop.py $w
+done
 
 python benchmarks/deap_mp.py
-
