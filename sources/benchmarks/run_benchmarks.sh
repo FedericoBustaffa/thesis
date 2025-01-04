1455d0485b804a2ca378ca571624353db3c0cc68
FedericoBustaffa <kiko.bustaffa@gmail.com>
2025-01-04 13:35:41 +0000

    deap

--- sources/benchmarks/run_benchmarks.sh
+++ sources/benchmarks/run_benchmarks.sh
@@ -5,6 +5,6 @@ python benchmarks/deap_mp.py MLPClassifier 0.0 --log=info
 python benchmarks/deap_mp.py SVC 0.0 --log=info
 python benchmarks/deap_mp.py RandomForestClassifier 0.0 --log=info
 
-python benchmarks/deap_mp.py MLPClassifier 0.1 hof --log=info
-python benchmarks/deap_mp.py SVC 0.1 hof --log=info
-python benchmarks/deap_mp.py RandomForestClassifier 0.1 hof --log=info
+python benchmarks/deap_mp.py MLPClassifier 0.1 --suffix=hof --log=info
+python benchmarks/deap_mp.py SVC 0.1 --suffix=hof --log=info
+python benchmarks/deap_mp.py RandomForestClassifier 0.1 --suffix=hof --log=info
