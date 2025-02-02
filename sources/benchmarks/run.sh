89da020b2bf06de2ccf1fe5608c57de33e1b724d
FedericoBustaffa <kiko.bustaffa@gmail.com>
2025-02-02 23:23:36 +0000

    script

--- sources/benchmarks/run.sh
+++ sources/benchmarks/run.sh
@@ -3,22 +3,22 @@
 
 # DEAP TEST
 # population
-python benchmarks/deap_pop.py MLPClassifier --suffix=pop --log=info
-python benchmarks/deap_pop.py SVC --suffix=pop --log=info
-python benchmarks/deap_pop.py RandomForestClassifier --suffix=pop --log=info
+python benchmarks/deap_pop.py MLPClassifier --suffix=final --log=info
+python benchmarks/deap_pop.py SVC --suffix=final --log=info
+python benchmarks/deap_pop.py RandomForestClassifier --suffix=final --log=info
 
 # feature
-python benchmarks/deap_ft.py MLPClassifier --suffix=feature --log=info
-python benchmarks/deap_ft.py SVC --suffix=feature --log=info
-python benchmarks/deap_ft.py RandomForestClassifier --suffix=feature --log=info
+python benchmarks/deap_ft.py MLPClassifier --suffix=final --log=info
+python benchmarks/deap_ft.py SVC --suffix=final --log=info
+python benchmarks/deap_ft.py RandomForestClassifier --suffix=final --log=info
 
 # PPGA TEST
 # population
-python benchmarks/ppga_pop.py MLPClassifier --suffix=pop --log=info
-python benchmarks/ppga_pop.py SVC --suffix=pop --log=info
-python benchmarks/ppga_pop.py RandomForestClassifier --suffix=pop --log=info
+python benchmarks/ppga_pop.py MLPClassifier --suffix=final --log=info
+python benchmarks/ppga_pop.py SVC --suffix=final --log=info
+python benchmarks/ppga_pop.py RandomForestClassifier --suffix=final --log=info
 
 # feature
-python benchmarks/ppga_ft.py MLPClassifier --suffix=feature --log=info
-python benchmarks/ppga_ft.py SVC --suffix=feature --log=info
-python benchmarks/ppga_ft.py RandomForestClassifier --suffix=feature --log=info
+python benchmarks/ppga_ft.py MLPClassifier --suffix=final --log=info
+python benchmarks/ppga_ft.py SVC --suffix=final --log=info
+python benchmarks/ppga_ft.py RandomForestClassifier --suffix=final --log=info
