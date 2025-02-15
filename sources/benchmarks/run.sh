1c0d8b41477634f0ced819fd0366086b2d40e2d3
FedericoBustaffa <kiko.bustaffa@gmail.com>
2025-02-15 09:54:02 +0000

    rf

--- sources/benchmarks/run.sh
+++ sources/benchmarks/run.sh
@@ -2,10 +2,10 @@
 
 
 # DEAP population
-python benchmarks/deap_pop.py $1 --suffix=final2 --log=info
+# python benchmarks/deap_pop.py $1 --suffix=final2 --log=info
 
 # DEAP feature
-python benchmarks/deap_ft.py $1 --suffix=final2 --log=info
+# python benchmarks/deap_ft.py $1 --suffix=final2 --log=info
 
 # PPGA population
 python benchmarks/ppga_pop.py $1 --suffix=final2 --log=info
